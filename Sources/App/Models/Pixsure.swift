8b1bbaae51852f86d45c773009f262b53d115112
Aaron Bikis <aaron.bikis@gmail.com>
2017-05-09 18:12:26 -0700

    can post data

--- Sources/App/Models/Pixsure.swift
+++ Sources/App/Models/Pixsure.swift
@@ -4,56 +4,82 @@ import Vapor
 struct Pixsure: Model {
   var exists: Bool = false
   var id: Node?
+  var modificationDate: String
   let imageURL: String
-  let hotspotLocations: [String]
+  let hotspotLocation: HotSpotLocation
   
-  init(imageURL: String, hotspotLocations:[String]){
-    
+  init(imageURL: String, hotspotLocations: HotSpotLocation, modificationDate: String){
+    self.modificationDate = modificationDate
     self.imageURL = imageURL
-    self.hotspotLocations = hotspotLocations
+    self.hotspotLocation = hotspotLocations
   }
   
   init(node: Node, in context: Context) throws {
-    
-    id = try node.extract("id")
+    //    id = try node.extract("id")
+    modificationDate = try node.extract("modificationDate")
     imageURL = try node.extract("imageURL")
-    hotspotLocations = try node.extract("hotspotLocations")
+    hotspotLocation = try node.extract("hotspotLocations")
+  }
+  
+  static func prepare(_ database: Database) throws {
+    //Using MongoDB so leave this unimplemented
   }
   
   func makeNode(context: Context) throws -> Node {
     
-    return try Node(node: ["id": id])
+    return try Node(node: ["modificationDate": modificationDate,
+                     "imageURL": imageURL,
+                     "hotSpotLocation": hotspotLocation.makeNode()])
+  }
+  
+  static func revert(_ database: Database) throws {
+    try database.delete("Pixsures")
+  }
+}
+
+struct HotSpotLocation: Model {
+  var id: Node?
+  var type: String
+  var param: String
+  
+  init(type: String, param: String){
+    self.type = type
+    self.param = param
+  }
+  
+  init(node: Node, in context: Context) throws {
+    type = try node.extract("type")
+    param = try node.extract("param")
   }
   
   static func prepare(_ database: Database) throws {
-    
-    try database.create("pixsures", closure: { (pixsure) in
-      pixsure.id()
-      pixsure.string("imageURL")
-      
-      pixsure.
-    })
+    // do nothing
   }
   
+  func makeNode(context: Context) throws -> Node {
+    return try Node(node: ["type": type,
+                           "param": param])
+  }
   
   static func revert(_ database: Database) throws {
-    try database.delete("pixsures")
+    try database.delete("hotSpotLocation")
   }
-  
   
   
 }
 
-enum HotSpotLocation {
-  case topLeft
-  case topMid
-  case topRight
-  case midLeft
-  case midRight
-  case botLeft
-  case botMid
-  case botRight
-}
+//
+//enum HotSpotLocation {
+//  case topLeft
+//  case topMid
+//  case topRight
+//  case midLeft
+//  case midRight
+//  case botLeft
+//  case botMid
+//  case botRight
+//}
+
 
 enum TapFunctionality {
   case share
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,8 +3,32 @@ import VaporMongo
 
 let drop = Droplet()
 
-try drop.addProvider(VaporMongo.Provider.self)
+drop.preparations.append(Pixsure.self)
 
+do {
+  try drop.addProvider(VaporMongo.Provider.self)
+} catch {
+  assertionFailure("error adding provider \(error)")
+}
+
+
+//MARK: GET
+drop.get("pixsures") { req in
+  let pixsures = try Pixsure
+    .all()
+    .makeNode()
+  
+  let nodeDict = ["pixsures": pixsures]
+  return try JSON(node: nodeDict)
+}
+
+
+drop.post("pixsure") { req in
+  var pixsure = try Pixsure(node: req.json)
+  try pixsure.save()
+  return try pixsure.makeJSON()
+}
+  
 drop.resource("posts", PostController())
-
+  
 drop.run()
