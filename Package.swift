7cddb74f29aa4a22ffd6a47eb84489d68528d564
Aaron Bikis <aaron.bikis@gmail.com>
2017-05-10 10:33:29 -0700

    update dependencies

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ let package = Package(
     name: "OnYourFeet",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
-        .Package(url: "https://github.com/vapor/mongo-provider.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/vapor/mongo-provider.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/nodes-vapor/aws.git", majorVersion: 0)
     ],
     exclude: [
         "Config",
